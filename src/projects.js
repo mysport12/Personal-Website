865c16c771fac5599f342e66fdad5d9770c79820
Craig Martin <cmart1220@gmail.com>
2017-04-26 17:45:44 -0400

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -1,46 +1,41 @@
-import React from 'https://unpkg.com/react@15/dist/react.min.js'
-import ReactDOM from 'https://unpkg.com/react-dom@15/dist/react-dom.min.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import card from 'bootstrap-css'
 
 //Projects page
 
-function Project() {
+class Project extends React.Component {
+    render() {
+        const pcjband = {
+            name: "Pace Car Joe",
+            url: "www.pcjband.com",
+            img: "/img/pacecarjoe.jpg",
+            description: "My friend reached out looking for help with his band's website. I set him up with a Wordpress site that he is able to manage and maintain on his own. The project provided me with my first look at Wordpress and the user friendly admin dashboard."
+        };
 
-	const pcjband = {
-		name: "Pace Car Joe",
-		url: "www.pcjband.com",
-		img: "/img/pacecarjoe.jpg",
-		description: "My friend reached out looking for help with his band's website. I set him up with a Wordpress site that he is able to manage and maintain on his own. The project provided me with my first look at Wordpress and the user friendly admin dashboard."
-	};
+        const dailyem = {
+            name: "Daily Em",
+            url: "www.dailyem.com",
+            img: "/img/dailyem.jpg",
+            description: "DailyEm.com is my wife's blog created to provide readers with fitness ideas/motivation, new and exciting recipes, and funny stories about our two dogs."
+        };
 
-	const dailyem = {
-		name: "Daily Em",
-		url: "www.dailyem.com",
-		img: "/img/dailyem.jpg",
-		description: "DailyEm.com is my wife's blog created to provide readers with fitness ideas/motivation, new and exciting recipes, and funny stories about our two dogs."
-	};
-
-	return (
-		<div styleName='card'>
-			<div styleName='card-block'>
-				<h3 styleName='card-title'>{dailyem.name}</h3>
-				<p styleName='card-text'>{dailyem.url}</p>
-				<img src={dailyem.img} styleName='card-img-left'/>
-				<p>{dailyem.description}</p>
-			</div>
-			<div styleName="row">
-				<h3>{pcjband.name}</h3>
-				<p>{pcjband.url}</p>
-				<img src={pcjband.img}/>
-				<p>{pcjband.description}</p>
-			</div>
-		</div>
-	);
+        return (
+            <div styleName='card'>
+                <div styleName='card-block'>
+                    <h3 styleName='card-title'>{dailyem.name}</h3>
+                    <p styleName='card-text'>{dailyem.url}</p>
+                    <img src={dailyem.img} styleName='card-img-left'/>
+                    <p>{dailyem.description}</p>
+                </div>
+                <div styleName="row">
+                    <h3>{pcjband.name}</h3>
+                    <p>{pcjband.url}</p>
+                    <img src={pcjband.img}/>
+                    <p>{pcjband.description}</p>
+                </div>
+            </div>
+        );
 }
 
-function DisplayProjects() {
-	ReactDOM.render(
-		<Project />,
-		document.getElementById('content')
-	);
-}
+module.exports = Project;
